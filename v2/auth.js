2a066fffbeca568278faa2e6aa78097726bb7ba3
akhilpatel7 <akhilpatel.design@gmail.com>
2026-04-13 21:18:48 -0700

    v2 remove

--- v2/auth.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  var expiry = localStorage.getItem("ap_auth");
-  if (!expiry || Date.now() > Number(expiry)) {
-    localStorage.removeItem("ap_auth");
-    window.location.replace("/v2");
-  }
-})();
